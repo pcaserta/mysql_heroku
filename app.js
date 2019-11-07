7b237a5524b9adc2822f7a55e8c16e77e3703504
pcaserta <patrick.caserta@live.longwood.edu>
2019-11-07 16:41:17 -0500

    fixed port

--- app.js
+++ app.js
@@ -24,5 +24,5 @@ app.use(router);
 //server runnig on port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`app listening on port ${port}...`);
+  console.log(`app listening on port ${PORT}...`);
 });
